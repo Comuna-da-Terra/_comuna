7a822989ffdf13c803fded1033526a84823f1680
Murillo Miguel <82779468+psiqe@users.noreply.github.com>
2024-09-02 17:36:56 -0300

    Update api.js

--- ui_web/src/services/api.js
+++ ui_web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://comunadaterra.com.br/api",
+    baseURL: "http://api.comunadaterra.com.br/api",
     //baseURL: "http://127.0.0.1:8000/api",
     // baseURL: "http://192.168.1.103:8000/api",
     headers: {
