d34f67ca1a493b9ef36970d453e1bcc7078b11b8
Murillo Miguel <mmiguel.skn@gmail.com>
2024-10-15 10:05:31 -0300

    aplicado transaction para garantir que politicas sejama ceitas quando usuario for criado

--- api/accounts/serializer.py
+++ api/accounts/serializer.py
@@ -3,6 +3,8 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 from .models import User
+from policies.models import UserPolicy, Policy
+from django.db import transaction
 
 from django.contrib.auth.hashers import make_password
 from .utils.random_username import random_username
@@ -10,6 +12,7 @@ from hashlib import sha256
 
 
 class UserSerializer(ModelSerializer):
+    policy = serializers.BooleanField(write_only=True, required=True)
     class Meta:
         model               = User
         fields              = [
@@ -23,9 +26,11 @@ class UserSerializer(ModelSerializer):
             "coop_number",
             "is_superuser",
             "is_staff",
-            "is_active"
+            "is_active",
+            "policy"
             ] 
         read_only_fields    = ["created_at", "updated_at"]
+
         extra_kwargs        = {
             "password": {"write_only": True},
             "email": {
@@ -33,14 +38,31 @@ class UserSerializer(ModelSerializer):
             },
         }
 
-    def create(self, validated_data) -> User:
-        member              = 'coop_number' in validated_data
+    def create(self, validated_data):
+            policy_accepted = validated_data.pop('policy', False)
+
+            with transaction.atomic():
+                client          = User.objects.create(**validated_data)
+                
+                termos = Policy.objects.filter(type="termos")
+                privacidade = Policy.objects.filter(type="privacidade")
+                
+                if not termos.exists() or not privacidade.exists():
+                    raise ValueError("As políticas necessárias não foram encontradas.")
+                    
+        # member              = 'coop_number' in validated_data
     
-        if member:
-            member          = User.objects.create(**validated_data)
-            return member
-        else:
-            client          = User.objects.create(**validated_data)
+        # if member:
+        #     member          = User.objects.create(**validated_data)
+        #     return member
+        # else:
+            
+            if policy_accepted is True:
+                user_policy_termos = UserPolicy.objects.create(user=client, policy=termos[0], approval=True)
+                user_policy_privacidade = UserPolicy.objects.create(user=client, policy=privacidade[0], approval=True)
+            else:
+                 raise ValueError("As políticas necessárias não foram aceitas!")
+            
             return client
              
 
